462dbb32b12b7dbfb33ec71f6bb64e0791011a02
Fenyyx <fenixg12@gmail.com>
2025-02-14 14:44:22 +0000

    Terminadísimo

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 
 const Dice = () => {
     // Definimos state para guardar la imagen actual
-    const [image, setImage] = useState(`src/assets/images/dice-empty.png`);
+    const random = Math.floor(Math.random() * 6) + 1 //Le hacemos elegir un número del 1 al 6
+    const [image, setImage] = useState(`src/assets/images/dice${random}.png`);
 
     // Creamos una función para actualizar la imagen
     const randomDice = () => {
-        setImage(`src/assets/images/dice-empty.png`) //Comenzamos con el valor vacío
+
+        setImage(`src/assets/images/dice-empty.png`) //Comenzamos con el valor vacío que estará 1s
         setTimeout(() => {
-            const random = Math.floor(Math.random() * 6) + 1 //Le hacemos elegir un número del 1 al 6
-            setImage(`src/assets/images/dice${random}.png`)
+            setImage(`src/assets/images/dice${random}.png`) //le damos el resultado final
         }, 1000) // Le ponemos un delay de 1s
     }
 
